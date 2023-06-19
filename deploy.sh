b15a6a778ecddee876489e0d3e4267db66f3938f
dmitriygrosh <dmitriy.grosh@gmail.com>
2023-06-19 23:11:00 +0300

    add add prefix

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 yarn build
 
 echo "Deploying files to server..."
-scp -r dist/* grosh@194.67.110.39:/var/www/suitup/back
+scp -r dist/* grosh@194.67.110.39:/var/www/suitup/suitup_back/dist
 
 echo "Done!"
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-	app.enableCors();
+	app.enableCors({
+		origin: [
+			'http://localhost:3000',
+			'http://194.67.110.39/',
+			'http://suit-up.ru/'
+		]
+	});
+
+	app.setGlobalPrefix('api');
 
   await app.listen(3001);
 }
