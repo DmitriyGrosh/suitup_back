90cb87cbaadbc3cc5c8b5536ef0d5e4e7f5caefc
dmitriygrosh <dmitriy.grosh@gmail.com>
2023-06-19 23:44:17 +0300

    add add prefix

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: [
-			'http://localhost:3000',
-			'http://194.67.110.39/',
-			'http://suit-up.ru/'
-		]
+		allowedHeaders: ['content-type'],
+		origin: true
 	});
 
 	app.setGlobalPrefix('api');
