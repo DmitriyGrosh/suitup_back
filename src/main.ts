a2d234960887fd4e4c06b6f79f8b80bb20b095cc
dmitriygrosh <dmitriy.grosh@gmail.com>
2023-06-20 00:04:09 +0300

    add add prefix

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,30 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+	const whitelist = ['http://localhost:3000/', 'http://194.67.110.39/', 'http://suit-up.ru', 'http://suit-up.ru/', 'http://www.suit-up.ru'];
 	app.enableCors({
-		allowedHeaders: 'Access-Control-Allow-Origin',
+		origin: function (origin, callback) {
+			if (whitelist.indexOf(origin) !== -1) {
+				console.log("allowed cors for:", origin)
+				callback(null, true)
+			} else {
+				console.log("blocked cors for:", origin)
+				callback(new Error('Not allowed by CORS'))
+			}
+		},
+		allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+		methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
 		credentials: true,
-		origin: [
-			'http://localhost:3000/',
-			'http://194.67.110.39/',
-			'http://suit-up.ru'
-		]
 	});
+	// app.enableCors({
+	// 	allowedHeaders: 'Access-Control-Allow-Origin',
+	// 	credentials: true,
+	// 	origin: [
+	// 		'http://localhost:3000/',
+	// 		'http://194.67.110.39/',
+	// 		'http://suit-up.ru'
+	// 	]
+	// });
 
 	app.setGlobalPrefix('api');
 
