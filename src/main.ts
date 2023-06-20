70b64ea87c01b63e43d3f60adbe7522630aaf933
dmitriygrosh <dmitriy.grosh@gmail.com>
2023-06-20 20:48:20 +0300

    update headers

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,30 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-	const whitelist = ['http://localhost:3000', 'http://194.67.110.39/', 'http://suit-up.ru', 'http://suit-up.ru/', 'http://www.suit-up.ru'];
-	app.enableCors({
-		origin: function (origin, callback) {
-			if (whitelist.indexOf(origin) !== -1) {
-				console.log("allowed cors for:", origin)
-				callback(null, true)
-			} else {
-				console.log("blocked cors for:", origin)
-				callback(new Error('Not allowed by CORS'))
-			}
-		},
-		allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-		methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-		credentials: true,
-	});
+  const whitelist = [
+    'http://localhost:3000',
+    'http://194.67.110.39/',
+    'http://suit-up.ru',
+    'http://suit-up.ru/',
+    'http://www.suit-up.ru',
+  ];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization, Access-Control-Allow-Origin',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
 
-	app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
 
   await app.listen(3001);
 }
